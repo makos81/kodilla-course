6306be92b814a2b0f2b2434611dfb5eedcd4ed7f
Maciej Ra <maciejrajcic@gmail.com>
2020-12-09 21:45:51 +0100

    6.2 Adding Calculator class and its tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,22 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(3,4);
+        if(addResult==7){
+            System.out.println("Poprawny wynik dodawania");
+        }else{
+            System.out.println("Niepoprawny wynik dodawania");
+        }
+
+        int substractResult = calculator.subtract(2,4);
+        if(substractResult==-2){
+            System.out.println("Poprawny wynik odejmowania");
+        }else{
+            System.out.println("Niepoprawny wynik odejmowania");
+        }
     }
 }
