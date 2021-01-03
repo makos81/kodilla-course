e3739012e311c8c4f3a6bd9e1b8304bf5d36a6a9
Maciej Ra <maciejrajcic@gmail.com>
2021-01-03 13:53:02 +0100

    Adding 8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,34 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+metoda findFilght(Flight flight)
+wewnątrz metody zaimplementuj mapę typu HashMap, której kluczem będzie String przechowujący nazwę lotniska,
+a wartością Boolean informujący o tym czy można na dane lotnisko polecieć. Przykład: Map<String, Boolean.
+logika powinna wyszukiwać loty w mapie. Jeżeli dane lotnisko nie będzie istnieć w mapie, należy rzucić \
+wyjątkiem RouteNotFoundException.
+ */
+public class FlightFinder {
+    private Map<String, Boolean> flightMap;
+
+    public FlightFinder(){
+        flightMap = new HashMap<>();
+        flightMap.put("Berlin", true);
+        flightMap.put("Warsaw", true);
+        flightMap.put("London", true);
+    }
+
+    public boolean findFlight(Flight flight) throws RouteNotFoundException{
+        boolean departureAirport = false;
+        boolean arrivalAirport = false;
+        try{
+            arrivalAirport = flightMap.get(flight.getArrivalAirport());
+            departureAirport = flightMap.get(flight.getDepartureAirport());
+        }catch(Exception message){
+            throw new RouteNotFoundException("Airport not found ");
+        }
+        return departureAirport&&arrivalAirport;
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.test;
+
+public class FlightRunner {
+    public static void main(String[] args){
+        Flight flight = new Flight("Oslo", "Warsaw");
+        FlightFinder flightFinder = new FlightFinder();
+        boolean foundFlight = false;
+        try{
+            foundFlight = flightFinder.findFlight(flight);
+        }catch (RouteNotFoundException e){
+            System.out.println("Route not found " + e);
+        }
+        System.out.println("Can flight? " + foundFlight);
+    }
+}
