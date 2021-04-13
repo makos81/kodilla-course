5d4ee3a64c7a34639047467977e31f3bb658254c
makos81 <maciejrajcic@gmail.com>
2021-04-13 21:58:31 +0200

    Adding testing2 exc 1

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,2 +1,19 @@
-package com.kodilla.testing2.ebay;public class EBayTestingApp {
+package com.kodilla.testing2.ebay;
+
+import com.kodilla.testing2.google.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class EBayTestingApp {
+    public static final String SEARCHFIELD = "q";                       // [1]
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.ebay.com");
+
+        WebElement searchField = driver.findElement(By.cssSelector("[class='gh-tb ui-autocomplete-input']"));
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
 }
--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,2 +1,19 @@
-package com.kodilla.testing2.google;public class GoogleTestingApp {
+package com.kodilla.testing2.google;
+
+import com.kodilla.testing2.google.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleTestingApp {
+    public static final String SEARCHFIELD = "q";                       // [1]
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX); // [2]
+        driver.get("https://www.google.com");                                 // [3]
+
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));      // [4]
+        searchField.sendKeys("Kodilla");                                      // [5]
+        searchField.submit();                                                 // [6]
+    }
 }
